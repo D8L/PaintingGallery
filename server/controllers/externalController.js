db1c0dc29bca3fafea9c31a4041548d0d31816a7
D8L <101610340+D8L@users.noreply.github.com>
2023-08-01 00:05:46 -0400

    email testing?

--- server/controllers/externalController.js
+++ server/controllers/externalController.js
@@ -6,7 +6,7 @@ const server = require('../server');
 
 const searchPaintings = async (req, res) => {
     try {
-    //    console.log(server.sessionKey);     For when I upgrade to this API V2
+    //    console.log(server.sessionKey);     For when I upgrade to this API V2!
      //   console.log("https://www.wikiart.org/en/api/2/PaintingSearch?term=" + req.params.searchQuery + "&authSessionKey=" + server.sessionKey)
         const paintings = await Axios.get("http://www.wikiart.org/en/search/" + req.params.searchQuery + "/1?json=2&PageSize=1") // + "&authSessionKey=" + sessionKey)        res.json(paintings.data)
         res.json(paintings.data)
